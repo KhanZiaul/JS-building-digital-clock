399b9a0e59ad8575df66eee0a85cd999f1e6eb0d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-20 16:21:19 +0600

    DONE

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ function tick(){
     const seconds = now.getSeconds();
 
     const CreateHtml = `
+    <p class= "text-7xl mb-10"> DIGITAL CLOCK <p>
+
+    <div class = "border-2 rounded-xl p-2 bg-sky-900 lg:w-[50%] mx-auto lg:p-5">
     <span> ${hours} : </span>
     <span> ${minutes} : </span>
     <span> ${seconds}</span>
+    </div>
     `;
   
     clock.innerHTML = CreateHtml;
